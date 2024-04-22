06f9eb9a1c65ffd32a8151973b75621d1d6272b5
Devarsh01 <devarsh.s2@ahduni.edu.in>
2024-04-22 13:20:17 -0400

    Pre-processing done!

--- project_phaseI.py
+++ project_phaseI.py
@@ -5,6 +5,11 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+from scipy.stats import shapiro
+
+#%%
+title_style = {'fontname': 'serif', 'color': 'blue', 'size': 'large'}
+label_style = {'fontname': 'serif', 'color': 'darkred'}
 
 #%%
 df = pd.read_csv("C:/Users/DEVARSH SHETH/Desktop/GWU/Data_Visulization/Project/Data_Viz_Project/exported_data.csv")  # Replace "your_dataset.csv" with the actual file path
@@ -53,20 +58,21 @@ print(df_cleaned.head())
 print("\nStatistics of the cleaned dataset:")
 print(df_cleaned.describe())
 # %%
-numerical_columns = ['Age', 'Average In-game purchase', 'Negative', 'Peak CCU', 
-                     'Positive', 'Price', 'Revenue', 'User score']
+df_cleaned['Age'] = df_cleaned['Age'].astype(int)
+
+numerical_columns = df_cleaned.select_dtypes(include=['float64', 'int64']).columns
 
 # Create box plots for each numerical column to visualize outliers
 plt.figure(figsize=(16, 10))
 for i, column in enumerate(numerical_columns, 1):
-    plt.subplot(3, 3, i)
+    plt.subplot(4, 4, i)
     sns.boxplot(y=df_cleaned[column])
     plt.title(column)
 plt.tight_layout()
 plt.show()
 
 # %%
-features = df_cleaned.select_dtypes(include=['float64', 'int64'])
+features = df_cleaned[numerical_columns]
 
 # Standardize the features
 scaler = StandardScaler()
@@ -93,8 +99,6 @@ condition_number = np.linalg.cond(scaled_features)
 print("Condition Number:", condition_number)
 
 # %%
-from scipy.stats import shapiro
-
 # Perform Shapiro-Wilk test for normality
 statistic, p_value = shapiro(features)
 
@@ -108,5 +112,33 @@ if p_value > alpha:
     print("The data looks Gaussian (fail to reject H0)")
 else:
     print("The data does not look Gaussian (reject H0)")
+
 # %%
 
+correlation_matrix = df_cleaned[numerical_columns].corr()
+plt.figure(figsize=(12, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title("Pearson Correlation Coefficient Matrix", **title_style)
+plt.xlabel("Variables", **label_style)
+plt.ylabel("Variables", **label_style)
+plt.gca().tick_params(axis='both', which='major', labelsize=10)
+plt.show()
+
+# %%
+
+fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(25, 15))
+
+# Plot KDE plots for each numerical variable
+for i, column in enumerate(features.columns):
+    if i >= 12:  # Plot only 12 KDE plots
+        break
+    sns.kdeplot(data=df_cleaned, x=column, fill=True, ax=axes.flatten()[i])
+    axes.flatten()[i].set_title(f"{column} Distribution", **title_style)
+    axes.flatten()[i].set_xlabel(column, **label_style)
+    axes.flatten()[i].set_ylabel("Density", **label_style)
+    axes.flatten()[i].tick_params(axis='both', which='major', labelsize=10)
+
+# Adjust layout
+plt.tight_layout()
+plt.show()
+# %%
